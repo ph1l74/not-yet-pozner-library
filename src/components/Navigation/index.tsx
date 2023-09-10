4ca11a0d89fce985b3e1f23eebc599b62888f1ed
Filat Astakhov <astakhovfilat@gmail.com>
2023-09-11 02:50:32 +0300

    Navigation appended

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,20 +1,31 @@
 import { NavLink } from "react-router-dom";
 import { CATEGORIES } from "@/constants";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 const Navigation = () => {
   const cats = CATEGORIES;
   return (
-    <nav className="h-full flex justify-start">
-      <ul className="flex h-full gap-4 items-center justify-start font-semibold lowercase">
+    <NavigationMenu>
+      <NavigationMenuList>
         {cats.map((cat, index) => {
           return (
-            <li key={`${cat.key}_${index}`}>
-              <NavLink to={`/${cat.key}`}>{cat.title}</NavLink>
-            </li>
+            <NavigationMenuItem key={`${cat.key}_${index}`}>
+              <NavLink to={`/${cat.key}`}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {cat.title}
+                </NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
           );
         })}
-      </ul>
-    </nav>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
 
